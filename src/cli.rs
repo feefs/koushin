566580ecc36dd1ff23a76ed690bdb8c7ec7be2e5
feefs <feefs@berkeley.edu>
2022-11-24 17:46:49 -0800

    bump deps + refactor to isolate clap command

--- src/cli.rs
+++ src/cli.rs
@@ -1,18 +1,14 @@
-use crate::{config, mal};
-
 use clap::{Parser, Subcommand};
-use eyre::{eyre, Result};
-use owo_colors::OwoColorize;
 
 #[derive(Parser)]
 #[clap(about, version)]
-struct Cli {
+pub struct Cli {
     #[clap(subcommand)]
-    command: Option<CliCommands>,
+    pub command: Option<CliCommands>,
 }
 
 #[derive(Subcommand)]
-enum CliCommands {
+pub enum CliCommands {
     /// Display your MyAnimeList in an organized format
     List,
     /// Set an attribute for an anime
@@ -33,48 +29,9 @@ enum CliCommands {
 }
 
 #[derive(Subcommand)]
-enum SetCommands {
+pub enum SetCommands {
     /// Set episode count
     Count,
     /// Set airing day
     Day,
-}
-
-pub fn koushin() -> Result<()> {
-    if !cfg!(unix) {
-        return Err(eyre!("Not on Unix!"));
-    }
-
-    let cli = Cli::parse();
-    match &cli.command {
-        Some(command) => match command {
-            CliCommands::List => mal::display_currently_watching_list()?,
-            CliCommands::Set { set_command } => {
-                match set_command {
-                    SetCommands::Count => mal::update_episode_count(mal::EpisodeAction::Set)?,
-                    SetCommands::Day => mal::update_airing_day()?,
-                };
-                println!("{}", "更新されました!".green());
-            }
-            CliCommands::Mal => mal::open_my_anime_list()?,
-            CliCommands::Page => mal::open_anime_page()?,
-            CliCommands::Config { set_client } => {
-                if *set_client {
-                    config::set_client_config()?;
-                } else {
-                    let path = match config::config_folder_path()?.into_os_string().into_string() {
-                        Ok(p) => p,
-                        Err(_) => return Err(eyre!("Unable to convert OsString to String!")),
-                    };
-                    println!("{}", path);
-                }
-            }
-        },
-        None => {
-            mal::update_episode_count(mal::EpisodeAction::Increment)?;
-            println!("{}", "更新されました!".green());
-        }
-    }
-
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,47 @@ mod cli;
 mod config;
 mod mal;
 
-use crate::cli::koushin;
-
+use eyre::{eyre, Result};
 use owo_colors::OwoColorize;
+
+fn koushin() -> Result<()> {
+    if !cfg!(unix) {
+        return Err(eyre!("Not on Unix!"));
+    }
+
+    let c = <cli::Cli as clap::Parser>::parse();
+    match &c.command {
+        Some(command) => match command {
+            cli::CliCommands::List => mal::display_currently_watching_list()?,
+            cli::CliCommands::Set { set_command } => {
+                match set_command {
+                    cli::SetCommands::Count => mal::update_episode_count(mal::EpisodeAction::Set)?,
+                    cli::SetCommands::Day => mal::update_airing_day()?,
+                };
+                println!("{}", "更新されました!".green());
+            }
+            cli::CliCommands::Mal => mal::open_my_anime_list()?,
+            cli::CliCommands::Page => mal::open_anime_page()?,
+            cli::CliCommands::Config { set_client } => {
+                if *set_client {
+                    config::set_client_config()?;
+                } else {
+                    let path = match config::config_folder_path()?.into_os_string().into_string() {
+                        Ok(p) => p,
+                        Err(_) => return Err(eyre!("Unable to convert OsString to String!")),
+                    };
+                    println!("{}", path);
+                }
+            }
+        },
+        None => {
+            mal::update_episode_count(mal::EpisodeAction::Increment)?;
+            println!("{}", "更新されました!".green());
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
     std::process::exit(match koushin() {
